82da8fc2bab688768209bd5611057551aaa102c3
Aditya Jha <4075396+adtjha@users.noreply.github.com>
2023-03-21 13:45:35 +0530

    some progress.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.ogg')
+    }
+})
+const upload = multer({ dest: 'uploads/', preservePath: true, storage })
 const app = express()
 const port = 3000
 
@@ -14,6 +25,11 @@ app.get('/', (req, res) => {
             console.log('Sent:', fileName);
         }
     });
+})
+
+app.post('/audio', upload.single('audio'), (req, res) => {
+    console.log(req.file);
+    res.sendStatus(200);
 })
 
 app.listen(port, () => {
